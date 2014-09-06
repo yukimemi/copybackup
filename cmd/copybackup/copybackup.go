6018e3f6a72897c9cd40d3a41c0b05ec26889ec7
yukimemi <yukimemi@gmail.com>
2014-09-07 00:11:27 +0900

    Change help options. Using peco showHelp func. thanks peco.

--- cmd/copybackup/copybackup.go
+++ cmd/copybackup/copybackup.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"runtime"
 	"sync"
 
@@ -14,29 +16,72 @@ import (
 )
 
 type Options struct {
-	Generation int    `short:"g" long:"generation" description:"バックアップする世代。" default:"5"`
-	BackupDst  string `short:"b" long:"backup" description:"バックアップを保存する先。" default:"_old"`
-	Sleep      int    `short:"s" long:"sleep" description:"バックアップ間隔。" default:"60*5"`
+	Generation int    `short:"g" long:"generation" description:"バックアップする世代。(デフォルト: 5)" default:"5"`
+	BackupDst  string `short:"b" long:"backup" description:"バックアップを保存する先。(デフォルト: _old)" default:"_old"`
+	Sleep      int    `short:"s" long:"sleep" description:"バックアップ間隔。[秒] (デフォルト: 300)" default:"60*5"`
 	Csv        string `short:"c" long:"csv" description:"設定ファイルから読み込み。(csvファイル)"`
+	Help       bool   `short:"h" long:"help" description:"このヘルプを表示。"`
+}
+
+func showHelp() {
+
+	os.Stderr.WriteString(` 
+Usage: copybackup [options] [FILE | DIRECTORY | input csv file]
+
+Options:
+`)
+
+	t := reflect.TypeOf(Options{})
+	for i := 0; i < t.NumField(); i++ {
+		tag := t.Field(i).Tag
+
+		var o string
+		if s := tag.Get("short"); s != "" {
+			o = fmt.Sprintf("-%s, --%s", tag.Get("short"), tag.Get("long"))
+		} else {
+			o = fmt.Sprintf("--%s", tag.Get("long"))
+		}
+
+		fmt.Fprintf(
+			os.Stderr,
+			"  %-21s %s\n",
+			o,
+			tag.Get("description"),
+		)
+	}
 }
 
 func main() {
 	core.Logger, _ = log.NewLogger(os.Stdout, log.TIME_FORMAT_SEC, log.LOG_FORMAT_POWERFUL, log.LogLevel_Debug)
 
 	var e error
+	var st int
+	defer func() { os.Exit(st) }()
 	var wg sync.WaitGroup
 
 	cpus := runtime.NumCPU()
 	semaphore := make(chan int, cpus)
 
 	opts := new(Options)
-	parser := flags.NewParser(opts, flags.Default)
+	parser := flags.NewParser(opts, flags.PrintErrors)
+
 	args, e := parser.Parse()
-	core.FailOnError(e)
+	if e != nil {
+		st = 1
+		showHelp()
+		return
+	}
+
+	if opts.Help {
+		// parser.WriteHelp(os.Stderr)
+		showHelp()
+		return
+	}
 
 	if len(args) == 0 {
-		parser.WriteHelp(os.Stderr)
-		os.Exit(0)
+		// parser.WriteHelp(os.Stderr)
+		showHelp()
+		return
 	}
 
 	for _, arg := range args {
